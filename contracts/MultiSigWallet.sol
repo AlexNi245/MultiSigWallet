13d486d4937518e67ed9c1022e07e974984b7237
Alex Plutta <alex.plutta@googlemail.com>
2021-06-20 22:36:17 +0200

    add the functionality to approve transactions and proccess them

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -2,25 +2,126 @@
 
 pragma solidity ^0.8.0;
 
-import "./utils/open-zeppelin/Ownable.sol";
+contract MultiSigWallet {
+    address[] public owners;
+    mapping(address => bool) isOwner;
 
-contract MultiSigWallet is Ownable {
-    //Todo
-    //1. Constuctor How man sigs; How many approvals;
+    uint256 public necessaryApprovals;
+
+    mapping(uint256 => Transaction) public transactions;
+    uint256 public currentTransactionId;
+
+    struct Transaction {
+        address from;
+        address payable to;
+        uint256 ammount;
+        uint256 timestamp;
+        mapping(address => bool) hasApproved;
+        uint256 approvalCount;
+        bool isApproved;
+        bool isProccesed;
+    }
 
     modifier contructorGuard(
-        address[] memory owners,
-        uint256 
-        necessaryApprovals
+        address[] memory _owners,
+        uint256 _necessaryApprovals
     ) {
         require(
-            owners.length >= necessaryApprovals,
+            _owners.length >= _necessaryApprovals,
             "The number of approvals must me equal or less than the length of owners"
         );
         _;
     }
 
-    constructor(address[] memory owners, uint256 necessaryApprovals)
-        contructorGuard(owners, necessaryApprovals)
-    {}
+    modifier onlyOwner(address _sender) {
+        require(isOwner[_sender] == true, "You are not one of the owners");
+        _;
+    }
+
+    modifier singleApprovalGuard(address _sender, uint256 transactionId) {
+        require(
+            transactions[transactionId].hasApproved[_sender] == false,
+            "You already approved the transaction"
+        );
+        _;
+    }
+
+    modifier approvalGuard(uint256 _transactionId) {
+        require(
+            transactions[_transactionId].approvalCount >= necessaryApprovals,
+            "Necessary approvals are not reached"
+        );
+        _;
+    }
+
+    modifier proccessedGuard(uint256 _transactionId) {
+        require(
+            transactions[_transactionId].isProccesed == false,
+            "Double spending is not allowed"
+        );
+        _;
+    }
+
+    modifier balanceGuard(uint256 _amount) {
+        uint256 contractBalance = address(this).balance;
+        require(
+            _amount <= contractBalance,
+            "Trancaction ammount exceeds contracts balance"
+        );
+        _;
+    }
+
+    constructor(address[] memory _owners, uint256 _necessaryApprovals)
+        contructorGuard(_owners, _necessaryApprovals)
+    {
+        for (uint256 i = 0; i < _owners.length; i++) {
+            owners.push(_owners[i]);
+            isOwner[_owners[i]] = true;
+        }
+        necessaryApprovals = _necessaryApprovals;
+    }
+
+    function addTransaction(address payable _to, uint256 _ammount)
+        public
+        balanceGuard(_ammount)
+    {
+        Transaction storage transaction = transactions[currentTransactionId];
+
+        transaction.from = msg.sender;
+        transaction.to = _to;
+        transaction.ammount = _ammount;
+        transaction.timestamp = block.timestamp;
+
+        currentTransactionId++;
+    }
+
+    function approveTransaction(uint256 _transactionId)
+        public
+        onlyOwner(msg.sender)
+        singleApprovalGuard(msg.sender, _transactionId)
+    {
+        transactions[_transactionId].hasApproved[msg.sender] = true;
+        transactions[_transactionId].approvalCount++;
+
+        if (transactions[_transactionId].approvalCount >= necessaryApprovals) {
+            exectueTransaction(_transactionId, msg.sender);
+        }
+    }
+
+    function exectueTransaction(uint256 _transactionId, address _sender)
+        public
+        payable
+        onlyOwner(_sender)
+        balanceGuard(transactions[_transactionId].ammount)
+        approvalGuard(_transactionId)
+        proccessedGuard(_transactionId)
+    {
+        Transaction storage transaction = transactions[_transactionId];
+
+        transaction.isApproved = true;
+        transaction.isProccesed = true;
+        transaction.to.transfer(transaction.ammount);
+    }
+
+    receive() external payable {}
 }
--- test/MultiSigWallet.test.js
+++ test/MultiSigWallet.test.js
@@ -1,51 +1,433 @@
 
-const MultiSigWallet = artifacts.require( "./MultiSigWallet.sol");
+const MultiSigWallet = artifacts.require("./MultiSigWallet.sol");
 
 contract("MultiSigWallet", accounts => {
 
-
-
   it("constructor -- should reject invalid arguments", async () => {
 
-    const accounts = await web3.eth.getAccounts();
     const txParams = {
       from: accounts[0]
     };
 
-    try{
-      const instance = await MultiSigWallet.new([], 0, txParams);
-      assert.fail("Invalid arguments")
-    }catch(e){}
+    try {
+      const instance = await MultiSigWallet.new([], 1, txParams);
+    } catch (e) {
+      return true;
+    }
+
+    throw new Error("Contract was created despite wrong arguments!")
   });
 
   it("constructor -- number of owners are equal to approvals", async () => {
 
-    const accounts = await web3.eth.getAccounts();
     const txParams = {
       from: accounts[0]
     };
 
-    try{
+    try {
       const instance = await MultiSigWallet.new(["0x782C65D3721a49818CADaaF19869f00841103B52"], 1, txParams);
-    }catch(e){
+    } catch (e) {
+      console.log(e)
       assert.fail("Invalid arguments")
     }
   });
 
   it("constructor -- number of owners are greater as approvals", async () => {
 
-    const accounts = await web3.eth.getAccounts();
     const txParams = {
       from: accounts[0]
     };
 
-    try{
-      const instance = await MultiSigWallet.new(["0x782C65D3721a49818CADaaF19869f00841103B52","0x7761e957cc858F63be3B8A6f13e77c1295049513"], 1, txParams);
-    }catch(e){
+    try {
+      const instance = await MultiSigWallet.new(["0x782C65D3721a49818CADaaF19869f00841103B52", "0x7761e957cc858F63be3B8A6f13e77c1295049513"], 1, txParams);
+    } catch (e) {
       assert.fail("Invalid arguments")
     }
   });
 
+
+
+  it("constructor -- store owners properly", async () => {
+
+
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+
+
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+
+
+
+    assert.equal(await instance.owners(0), firstOwner)
+    assert.equal(await instance.owners(1), secondOwner)
+
+  })
+
+  it("approveTransaction -- fails if  not called by one of the owners", async () => {
+
+    const txParams = {
+      from: accounts[0]
+    };
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+    const dummyTransactionId = 0
+
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+
+    try {
+      await instance.approveTransaction(dummyTransactionId, { from: foreignAddress });
+
+    } catch (e) {
+      return true;
+
+    }
+    throw new Error("Function should reject if called by anyone else excepts the owners")
+  })
+
+
+  it("addTranaction -- fails if transaction ammount exceeds contract balance ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+
+
+    try {
+      await instance.addTransaction(firstOwner, 10000, { from: foreignAddress });
+    } catch (e) {
+      return true;
+    }
+
+    throw new Error("Function should reject if called by anyone else excepts the owners")
+
+  });
+
+  it("addTranaction -- correct transaction will be added to contracts transactions ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+
+
+    await instance.addTransaction(firstOwner, 100, { from: foreignAddress });
+
+    const transaction = await instance.transactions(0);
+
+    assert.equal(transaction.from, foreignAddress);
+    assert.equal(transaction.to, firstOwner);
+    assert.equal(transaction.approvalCount, 0);
+    assert.equal(transaction.isApproved, false);
+    assert.equal(transaction.isProccesed, false);
+
+
+
+  });
+
+
+  it("addTranaction -- current TransactionId will be incremented after a transaction was added ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+    await instance.addTransaction(firstOwner, 100, { from: foreignAddress });
+
+    const currentTransactionId = await instance.currentTransactionId();
+
+    assert.equal(currentTransactionId, 1);
+
+
+  });
+
+  it("approveTransaction -- fails if transaction ammount exceeds contract balance ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 900, { from: foreignAddress });
+    await instance.addTransaction(firstOwner, 900, { from: foreignAddress });
+
+    //Is successfull; 100 Wei are left 
+    await instance.approveTransaction(0);
+
+    try {
+      await instance.approveTransaction(1);
+    } catch (e) {
+      return true;
+    }
+
+
+    throw new Error("Contract should reject the second transaction");
+
+  });
+
+  it("approveTransaction -- fails if sender is not one of the owner ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+
+
+    try {
+      await instance.approveTransaction(0, { from: foreignAddress });
+
+    } catch (e) {
+      return true;
+    }
+
+
+    throw new Error("Contract should reject if sender is not the owner");
+
+  });
+
+  it("approveTransaction -- fails if sender tries to approve multiple times ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+
+
+    try {
+      await instance.approveTransaction(0, { from: firstOwner });
+      await instance.approveTransaction(0, { from: firstOwner });
+
+    } catch (e) {
+      return true;
+    }
+
+
+    throw new Error("Contract should reject if a owner tries to aprove multiple times");
+
+  });
+
+  it("approveTransaction -- dont submit transaction if necessary votes are not reached ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 2, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+    const oldBalance = await web3.eth.getBalance(foreignAddress);
+
+
+    await instance.approveTransaction(0, { from: firstOwner });
+
+    await instance.transactions(0);
+
+    const transaction = await instance.transactions(0);
+
+
+    assert.equal(transaction.isApproved, false);
+
+    const newBalance = await web3.eth.getBalance(foreignAddress);
+
+    assert.equal(Number.parseInt(newBalance), Number.parseInt(oldBalance));
+
+  });
+
+  it("approveTransaction -- submit transaction if necessary approvesare reached ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 2, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+    const oldBalance = await web3.eth.getBalance(foreignAddress);
+
+
+    await instance.approveTransaction(0, { from: firstOwner });
+    await instance.approveTransaction(0, { from: secondOwner });
+    await instance.transactions(0);
+
+    const transaction = await instance.transactions(0);
+
+
+    assert.equal(transaction.isApproved, true);
+
+    const newBalance = await web3.eth.getBalance(foreignAddress);
+
+    assert.equal(Number.parseInt(newBalance), Number.parseInt(oldBalance) + 10);
+
+  });
+
+  it("approveTransaction -- fails if transaction is already processed ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 2, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+
+
+    await instance.approveTransaction(0, { from: firstOwner });
+    await instance.approveTransaction(0, { from: secondOwner });
+    await instance.transactions(0);
+
+    const transaction = await instance.transactions(0);
+
+    assert.equal(transaction.isProccesed, true);
+
+    try {
+      await instance.executeTransaction(0, { from: secondOwner });
+    } catch (e) {
+
+      return true;
+    }
+
+    throw Error("Transaction should not be executed more than once")
+
+  });
+
+  it("approveTransaction -- fails if sender is not one of the owner ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+    await instance.send(1000, { from: firstOwner })
+
+    await instance.addTransaction(firstOwner, 10, { from: foreignAddress });
+
+
+
+
+    try {
+      await instance.approveTransaction(0, { from: foreignAddress });
+
+    } catch (e) {
+      return true;
+    }
+
+
+    throw new Error("Contract should reject if sender is not the owner");
+
+  });
+
+
+
+
+
+  it("execute Transaction -- fails if sender is not one of the owners ", async () => {
+    const txParams = {
+      from: accounts[0]
+    };
+
+
+    const firstOwner = accounts[0];
+    const secondOwner = accounts[1];
+    const foreignAddress = accounts[2];
+
+    const instance = await MultiSigWallet.new([firstOwner, secondOwner], 1, txParams);
+
+    try {
+      await instance.executeTransaction(0, { from: foreignAddress });
+
+    } catch (e) {
+      return true;
+    }
+
+    throw new Error("Contract should reject if sender is not the owner");
+
+  });
 
 
 
